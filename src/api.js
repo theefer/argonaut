83178a70029fa124d2ffc4529dd0d348e52db8ec
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-11-19 17:53:32 +0000

    More tests for Api

--- src/api.js
+++ src/api.js
@@ -1,9 +1,16 @@
 define(['./resource.js', './http/base.js'], function(ResourceFactory, Http) {
 
+    /**
+     * @param {object} options 
+     */
     function Api(uri, options) {
         this.uri = uri;
 
         options = options || {};
+
+        if (! uri) {
+            throw new Error('Missing URI parameter when creating Api');
+        }
 
         if (! options.httpAdapter) {
             throw new Error('Missing httpAdapter option when creating Api');
--- test/specs/api.spec.js
+++ test/specs/api.spec.js
@@ -1,25 +1,81 @@
 define(['/base/src/api.js', 'chai'], function(Api, chai) {
 
-    describe('Api', function(){
-        describe('constructor', function(){
+    describe('Api', function() {
+
+        function mockHttpAdapter() {
+            return {};
+        }
+
+        describe('constructor', function() {
             it('should be a function', function() {
                 Api.should.be.a('function');
             });
 
-            it('should throw an exception if called without an httpAdapter', function() {
+            it('should throw an exception if called without a URI', function() {
                 (function() {
                     new Api();
+                }).should.throw(Error, 'Missing URI parameter when creating Api');
+            });
+
+            it('should throw an exception if called without an httpAdapter', function() {
+                (function() {
+                    new Api('http://api.example.com/');
                 }).should.throw(Error, 'Missing httpAdapter option when creating Api');
             });
 
-            // TODO: httpAdapter
-            // TODO: syncData
+            it('should construct an Api instance', function() {
+                var api = new Api('http://api.example.com/', {
+                    httpAdapter: mockHttpAdapter()
+                });
+
+                api.should.be.an('object');
+            });
+
+            // TODO: accept syncData
         });
 
-        describe('#root', function(){
+        describe('#root', function() {
+            var api;
+
+            beforeEach(function() {
+                api = new Api('http://api.example.com/', {
+                    httpAdapter: mockHttpAdapter()
+                });
+            });
+
+            it('should be a function', function() {
+                api.root.should.be.a('function');
+            });
+
+            it('should return a Resource pointing to the API root', function() {
+                var root = api.root();
+
+                root.should.be.an.instanceof(api.Resource);
+                root.uri.should.equal('http://api.example.com/');
+            });
+
+            it('should return a new Resource every time it is called', function() {
+                var root1 = api.root();
+                var root2 = api.root();
+
+                root1.should.not.be.equal(root2);
+            });
         });
 
-        describe('#Resource', function(){
+        describe('#Resource', function() {
+            var api;
+
+            beforeEach(function() {
+                api = new Api('http://api.example.com/', {
+                    httpAdapter: mockHttpAdapter()
+                });
+            });
+
+            it('should be a (constructor) function', function() {
+                api.Resource.should.be.a('function');
+            });
+
+            // TODO: same http adapter, syncData option
         });
     });
 });
