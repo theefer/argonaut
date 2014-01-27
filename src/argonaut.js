4f925e1e4116284f91b3d2cfa9e258051710a5ba
Oliver Joseph Ash <oliverjash@icloud.com>
2014-01-27 21:42:06 +0000

    Move main file to src dir

--- src/argonaut.js
+++ src/argonaut.js
@@ -1,6 +1,6 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(['./src/api'], function (Api) {
+define(['./api'], function (Api) {
   return {
     Api: Api
   };
