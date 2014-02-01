b32b9aad60cc8952fc6e49b0feacbf76ef360e1e
Oliver Joseph Ash <oliverjash@icloud.com>
2014-02-01 23:15:48 +0000

    Cast promises in HTTP adapters using the injected promise adapter

--- src/http/reqwest.js
+++ src/http/reqwest.js
@@ -2,88 +2,93 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(['reqwest'], function(reqwest) {
 
-    function ajax(method, uri, data, options) {
-        options = options || {};
+    return function (options) {
+        var promiseAdapter = options.promiseAdapter;
 
-        // FIXME: defaults?
-        // if (!options.type) {
-        // }
+        function ajax(method, uri, data, options) {
+            options = options || {};
 
-        // if (!options.accept) {
-        // }
-
-        // if (data && !options.contentType) {
-        // }
-
-        var ajaxOptions = {
-            url:         uri,
-            method:      method,
-            data:        data,
-            type:        options.type,
-            accept:      options.accept,
-            contentType: data && options.contentType
-            // success: function(response) {
-            //     var request = req.request;
-            //     promise.resolve({
-            //         status:      request.status,
-            //         statusText:  request.statusText,
-            //         contentType: request.getResponseHeader('Content-Type'),
-            //         body:        response
-            //     });
-            // },
-            // error: function(request) {
-            //     // TODO: parse body if JSON
-            //     var error = {
-            //         status:      request.status,
-            //         statusText:  request.statusText,
-            //         contentType: request.getResponseHeader('Content-Type'),
-            //         body:        request.responseText
-            //     };
-            //     promise.reject(error);
+            // FIXME: defaults?
+            // if (!options.type) {
             // }
+
+            // if (!options.accept) {
+            // }
+
+            // if (data && !options.contentType) {
+            // }
+
+            var ajaxOptions = {
+                url:         uri,
+                method:      method,
+                data:        data,
+                type:        options.type,
+                accept:      options.accept,
+                contentType: data && options.contentType
+                // success: function(response) {
+                //     var request = req.request;
+                //     promise.resolve({
+                //         status:      request.status,
+                //         statusText:  request.statusText,
+                //         contentType: request.getResponseHeader('Content-Type'),
+                //         body:        response
+                //     });
+                // },
+                // error: function(request) {
+                //     // TODO: parse body if JSON
+                //     var error = {
+                //         status:      request.status,
+                //         statusText:  request.statusText,
+                //         contentType: request.getResponseHeader('Content-Type'),
+                //         body:        request.responseText
+                //     };
+                //     promise.reject(error);
+                // }
+            };
+
+            var req = reqwest(ajaxOptions);
+            var reqPromise = promiseAdapter.cast(req);
+
+            return reqPromise.then(function(response) {
+                var request = req.request;
+                return {
+                    status:      request.status,
+                    statusText:  request.statusText,
+                    contentType: request.getResponseHeader('Content-Type'),
+                    body:        response
+                };
+            }, function(request) {
+                return {
+                    status:      request.status,
+                    statusText:  request.statusText,
+                    contentType: request.getResponseHeader('Content-Type'),
+                    body:        request.responseText
+                };
+            });
+        }
+
+        function get(uri, data, options) {
+            return ajax('GET', uri, data, options);
+        }
+
+        function put(uri, data, options) {
+            return ajax('PUT', uri, data, options);
+        }
+
+        function post(uri, data, options) {
+            return ajax('POST', uri, data, options);
+        }
+
+        function del(uri, options) {
+            return ajax('DELETE', uri, options);
+        }
+
+        return {
+            get: get,
+            put: put,
+            post: post,
+            del: del
         };
-
-        var req = reqwest(ajaxOptions);
-
-        return req.then(function(response) {
-            var request = req.request;
-            return {
-                status:      request.status,
-                statusText:  request.statusText,
-                contentType: request.getResponseHeader('Content-Type'),
-                body:        response
-            };
-        }, function(request) {
-            return {
-                status:      request.status,
-                statusText:  request.statusText,
-                contentType: request.getResponseHeader('Content-Type'),
-                body:        request.responseText
-            };
-        });
-    }
-
-    function get(uri, data, options) {
-        return ajax('GET', uri, data, options);
-    }
-
-    function put(uri, data, options) {
-        return ajax('PUT', uri, data, options);
-    }
-
-    function post(uri, data, options) {
-        return ajax('POST', uri, data, options);
-    }
-
-    function del(uri, options) {
-        return ajax('DELETE', uri, options);
-    }
-
-    return {
-        get: get,
-        put: put,
-        post: post,
-        del: del
     };
 
 });
